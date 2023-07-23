d2f65de76abb42da512fa514c96559852005821a
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-07-23 23:49:26 +0300

    update

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled'
-
+export const Wrapper=styled.div`
+display:flex;
+flex-direction:column;`
 export const Button = styled.button`
   padding: 10px;
   width:150px;
